2d5cb8663b42466db32c9af694d11d7583751e85
Majkl <mpiksa@seznam.cz>
2025-09-18 21:35:44 +0200

    feat: add fixtures to create instances of pages

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -1,16 +1,10 @@
-import { test as setup } from "@playwright/test";
-import { LoginPage } from "../pages/login-page";
-import { DashboardPage } from "../pages/dashboard-page";
-import { standardUser } from "../data/credentials";
+import { test as setup } from "../fixtures/test-setup";
 
-
-setup('write login session data', async ({ page }) => {
-    const loginPage = new LoginPage(page, standardUser);
+setup('write login session data', async ({ loginPage, dashboardPage, page }) => {
     await loginPage.goto();
     await loginPage.checkPageTitle();
     await loginPage.loginToApp();
 
-    const dashboardPage = new DashboardPage(page);
     await page.waitForURL(dashboardPage.url);
     await dashboardPage.assertProductHeaderVisible();
 
