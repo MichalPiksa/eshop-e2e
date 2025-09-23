be4574707b6d9a385536ad6f9993ef0447ef8e83
Majkl <mpiksa@seznam.cz>
2025-09-23 22:00:02 +0200

    feat: add screenshot to dashboard pom and add base url variable

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -2,6 +2,7 @@ import { test } from "../fixtures/test-setup";
 
 test("Add to cart one item", async ({ dashboardPage }) => {
     await dashboardPage.goto();
+    await dashboardPage.captureScreenshot();
     await dashboardPage.assertProductHeaderVisible();
     await dashboardPage.addFirstProductToCart();
     await dashboardPage.checkItemsInCartButton(1);
