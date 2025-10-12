a2ecb8ec3878b7af292303e3ba211fc756136fc4
Majkl <mpiksa@seznam.cz>
2025-10-12 11:37:27 +0200

    feat: add screenshot assertion to login page

--- tests/logged-in/login.setup.ts
+++ tests/logged-in/login.setup.ts
@@ -3,6 +3,7 @@ import { test as setup } from "../../fixtures/test-setup";
 setup('write login session data', async ({ loginPage, dashboardPage, page }) => {
     await loginPage.goto();
     await loginPage.checkPageTitle();
+    await loginPage.loginButton.screenshot({ path: 'screenshots/login-page/login-button.png' });
     await loginPage.loginToApp();
 
     await page.waitForURL(dashboardPage.url);
