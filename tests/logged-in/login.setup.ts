e6b9186cd6b94a3d6356e0c601a91301acaba90e
Majkl <mpiksa@seznam.cz>
2025-10-17 22:07:08 +0200

    feat: add devices to config and snapshot validation

--- tests/logged-in/login.setup.ts
+++ tests/logged-in/login.setup.ts
@@ -1,13 +1,16 @@
-import { test as setup } from "../../fixtures/test-setup";
+import { expect, test as setup } from "../../fixtures/test-setup";
 
-setup('write login session data', async ({ loginPage, dashboardPage, page }) => {
+setup('write login session data', async ({ loginPage, dashboardPage, page }, testInfo) => {
     await loginPage.goto();
     await loginPage.checkPageTitle();
-    await loginPage.loginButton.screenshot({ path: 'screenshots/login-page/login-button.png' });
+    await expect(loginPage.loginButton).toHaveScreenshot("login-button.png");
     await loginPage.loginToApp();
 
     await page.waitForURL(dashboardPage.url);
     await dashboardPage.assertProductHeaderVisible();
 
-    await page.context().storageState({ path: ".auth/login.json" });
+    await dashboardPage.validateFullPageScreenshot();
+
+    const proj = (testInfo.project?.name ?? 'local').replace(/[:\s]/g, '-');
+    await page.context().storageState({ path: `.auth/${proj}.json` });
 });
